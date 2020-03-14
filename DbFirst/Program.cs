f0edd6114b0f8e63ff4991fc11cb11b2a4fcc6c0
tirupalu <c.subbathirupalu@gmail.com>
2020-03-14 14:05:10 +0530

    initial

---
+++ DbFirst/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbFirst
+{
+    public enum level : byte
+    {
+        Beginner=1,
+        Intermediate=2,
+        Advanced=3
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Context = new PlutoDbContext();
+         
+            var Courses = Context.GetCourses();
+            foreach(var x in Courses)
+            {
+                Console.WriteLine(x.Title);
+            }
+            Console.ReadLine();
+        }
+    }
+}
